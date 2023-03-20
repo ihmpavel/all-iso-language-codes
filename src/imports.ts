ca31bb1497fe4111c2eec6d54f7d0d2fba1deab6
Fernando Fernández <ferferga@hotmail.com>
2023-03-20 14:49:15 +0000

    chore: apply lint fixes

--- src/imports.ts
+++ src/imports.ts
@@ -1,172 +1,172 @@
 import aa from '../data/aa/639-1.json'
 import ab from '../data/ab/639-1.json'
+import ae from '../data/ae/639-1.json'
 import af from '../data/af/639-1.json'
 import ak from '../data/ak/639-1.json'
 import am from '../data/am/639-1.json'
-import ar from '../data/ar/639-1.json'
 import an from '../data/an/639-1.json'
+import ar from '../data/ar/639-1.json'
 import as from '../data/as/639-1.json'
 import av from '../data/av/639-1.json'
-import ae from '../data/ae/639-1.json'
 import ay from '../data/ay/639-1.json'
 import az from '../data/az/639-1.json'
 import ba from '../data/ba/639-1.json'
-import bm from '../data/bm/639-1.json'
 import be from '../data/be/639-1.json'
-import bn from '../data/bn/639-1.json'
-import bi from '../data/bi/639-1.json'
-import bo from '../data/bo/639-1.json'
-import bs from '../data/bs/639-1.json'
-import br from '../data/br/639-1.json'
 import bg from '../data/bg/639-1.json'
+import bi from '../data/bi/639-1.json'
+import bm from '../data/bm/639-1.json'
+import bn from '../data/bn/639-1.json'
+import bo from '../data/bo/639-1.json'
+import br from '../data/br/639-1.json'
+import bs from '../data/bs/639-1.json'
 import ca from '../data/ca/639-1.json'
-import cs from '../data/cs/639-1.json'
-import ch from '../data/ch/639-1.json'
 import ce from '../data/ce/639-1.json'
-import cu from '../data/cu/639-1.json'
-import cv from '../data/cv/639-1.json'
-import kw from '../data/kw/639-1.json'
+import ch from '../data/ch/639-1.json'
 import co from '../data/co/639-1.json'
 import cr from '../data/cr/639-1.json'
+import cs from '../data/cs/639-1.json'
+import cu from '../data/cu/639-1.json'
+import cv from '../data/cv/639-1.json'
 import cy from '../data/cy/639-1.json'
 import da from '../data/da/639-1.json'
 import de from '../data/de/639-1.json'
 import dv from '../data/dv/639-1.json'
 import dz from '../data/dz/639-1.json'
+import ee from '../data/ee/639-1.json'
 import el from '../data/el/639-1.json'
 import en from '../data/en/639-1.json'
 import eo from '../data/eo/639-1.json'
+import es from '../data/es/639-1.json'
 import et from '../data/et/639-1.json'
 import eu from '../data/eu/639-1.json'
-import ee from '../data/ee/639-1.json'
-import fo from '../data/fo/639-1.json'
 import fa from '../data/fa/639-1.json'
-import fj from '../data/fj/639-1.json'
+import ff from '../data/ff/639-1.json'
 import fi from '../data/fi/639-1.json'
+import fj from '../data/fj/639-1.json'
+import fo from '../data/fo/639-1.json'
 import fr from '../data/fr/639-1.json'
 import fy from '../data/fy/639-1.json'
-import ff from '../data/ff/639-1.json'
-import gd from '../data/gd/639-1.json'
 import ga from '../data/ga/639-1.json'
+import gd from '../data/gd/639-1.json'
 import gl from '../data/gl/639-1.json'
-import gv from '../data/gv/639-1.json'
 import gn from '../data/gn/639-1.json'
 import gu from '../data/gu/639-1.json'
-import ht from '../data/ht/639-1.json'
+import gv from '../data/gv/639-1.json'
 import ha from '../data/ha/639-1.json'
-import sh from '../data/sh/639-1.json'
 import he from '../data/he/639-1.json'
-import hz from '../data/hz/639-1.json'
 import hi from '../data/hi/639-1.json'
 import ho from '../data/ho/639-1.json'
 import hr from '../data/hr/639-1.json'
+import ht from '../data/ht/639-1.json'
 import hu from '../data/hu/639-1.json'
 import hy from '../data/hy/639-1.json'
-import ig from '../data/ig/639-1.json'
-import io from '../data/io/639-1.json'
-import ii from '../data/ii/639-1.json'
-import iu from '../data/iu/639-1.json'
-import ie from '../data/ie/639-1.json'
+import hz from '../data/hz/639-1.json'
 import ia from '../data/ia/639-1.json'
 import id from '../data/id/639-1.json'
+import ie from '../data/ie/639-1.json'
+import ig from '../data/ig/639-1.json'
+import ii from '../data/ii/639-1.json'
 import ik from '../data/ik/639-1.json'
+import io from '../data/io/639-1.json'
 import is from '../data/is/639-1.json'
 import it from '../data/it/639-1.json'
-import jv from '../data/jv/639-1.json'
+import iu from '../data/iu/639-1.json'
 import ja from '../data/ja/639-1.json'
-import kl from '../data/kl/639-1.json'
-import kn from '../data/kn/639-1.json'
-import ks from '../data/ks/639-1.json'
+import jv from '../data/jv/639-1.json'
 import ka from '../data/ka/639-1.json'
-import kr from '../data/kr/639-1.json'
-import kk from '../data/kk/639-1.json'
-import km from '../data/km/639-1.json'
-import ki from '../data/ki/639-1.json'
-import rw from '../data/rw/639-1.json'
-import ky from '../data/ky/639-1.json'
-import kv from '../data/kv/639-1.json'
 import kg from '../data/kg/639-1.json'
-import ko from '../data/ko/639-1.json'
+import ki from '../data/ki/639-1.json'
 import kj from '../data/kj/639-1.json'
+import kk from '../data/kk/639-1.json'
+import kl from '../data/kl/639-1.json'
+import km from '../data/km/639-1.json'
+import kn from '../data/kn/639-1.json'
+import ko from '../data/ko/639-1.json'
+import kr from '../data/kr/639-1.json'
+import ks from '../data/ks/639-1.json'
 import ku from '../data/ku/639-1.json'
-import lo from '../data/lo/639-1.json'
+import kv from '../data/kv/639-1.json'
+import kw from '../data/kw/639-1.json'
+import ky from '../data/ky/639-1.json'
 import la from '../data/la/639-1.json'
-import lv from '../data/lv/639-1.json'
+import lb from '../data/lb/639-1.json'
+import lg from '../data/lg/639-1.json'
 import li from '../data/li/639-1.json'
 import ln from '../data/ln/639-1.json'
+import lo from '../data/lo/639-1.json'
 import lt from '../data/lt/639-1.json'
-import lb from '../data/lb/639-1.json'
 import lu from '../data/lu/639-1.json'
-import lg from '../data/lg/639-1.json'
-import mh from '../data/mh/639-1.json'
-import ml from '../data/ml/639-1.json'
-import mr from '../data/mr/639-1.json'
-import mk from '../data/mk/639-1.json'
+import lv from '../data/lv/639-1.json'
 import mg from '../data/mg/639-1.json'
-import mt from '../data/mt/639-1.json'
-import mn from '../data/mn/639-1.json'
+import mh from '../data/mh/639-1.json'
 import mi from '../data/mi/639-1.json'
+import mk from '../data/mk/639-1.json'
+import ml from '../data/ml/639-1.json'
+import mn from '../data/mn/639-1.json'
+import mr from '../data/mr/639-1.json'
 import ms from '../data/ms/639-1.json'
+import mt from '../data/mt/639-1.json'
 import my from '../data/my/639-1.json'
 import na from '../data/na/639-1.json'
-import nv from '../data/nv/639-1.json'
-import nr from '../data/nr/639-1.json'
+import nb from '../data/nb/639-1.json'
 import nd from '../data/nd/639-1.json'
-import ng from '../data/ng/639-1.json'
 import ne from '../data/ne/639-1.json'
+import ng from '../data/ng/639-1.json'
 import nl from '../data/nl/639-1.json'
 import nn from '../data/nn/639-1.json'
-import nb from '../data/nb/639-1.json'
 import no from '../data/no/639-1.json'
+import nr from '../data/nr/639-1.json'
+import nv from '../data/nv/639-1.json'
 import ny from '../data/ny/639-1.json'
 import oc from '../data/oc/639-1.json'
 import oj from '../data/oj/639-1.json'
-import or from '../data/or/639-1.json'
 import om from '../data/om/639-1.json'
+import or from '../data/or/639-1.json'
 import os from '../data/os/639-1.json'
 import pa from '../data/pa/639-1.json'
 import pi from '../data/pi/639-1.json'
 import pl from '../data/pl/639-1.json'
-import pt from '../data/pt/639-1.json'
 import ps from '../data/ps/639-1.json'
+import pt from '../data/pt/639-1.json'
 import qu from '../data/qu/639-1.json'
 import rm from '../data/rm/639-1.json'
-import ro from '../data/ro/639-1.json'
 import rn from '../data/rn/639-1.json'
+import ro from '../data/ro/639-1.json'
 import ru from '../data/ru/639-1.json'
-import sg from '../data/sg/639-1.json'
+import rw from '../data/rw/639-1.json'
 import sa from '../data/sa/639-1.json'
+import sc from '../data/sc/639-1.json'
+import sd from '../data/sd/639-1.json'
+import se from '../data/se/639-1.json'
+import sg from '../data/sg/639-1.json'
+import sh from '../data/sh/639-1.json'
 import si from '../data/si/639-1.json'
 import sk from '../data/sk/639-1.json'
 import sl from '../data/sl/639-1.json'
-import se from '../data/se/639-1.json'
 import sm from '../data/sm/639-1.json'
 import sn from '../data/sn/639-1.json'
-import sd from '../data/sd/639-1.json'
 import so from '../data/so/639-1.json'
-import st from '../data/st/639-1.json'
-import es from '../data/es/639-1.json'
 import sq from '../data/sq/639-1.json'
-import sc from '../data/sc/639-1.json'
 import sr from '../data/sr/639-1.json'
 import ss from '../data/ss/639-1.json'
+import st from '../data/st/639-1.json'
 import su from '../data/su/639-1.json'
-import sw from '../data/sw/639-1.json'
 import sv from '../data/sv/639-1.json'
-import ty from '../data/ty/639-1.json'
+import sw from '../data/sw/639-1.json'
 import ta from '../data/ta/639-1.json'
-import tt from '../data/tt/639-1.json'
 import te from '../data/te/639-1.json'
 import tg from '../data/tg/639-1.json'
-import tl from '../data/tl/639-1.json'
 import th from '../data/th/639-1.json'
 import ti from '../data/ti/639-1.json'
-import to from '../data/to/639-1.json'
-import tn from '../data/tn/639-1.json'
-import ts from '../data/ts/639-1.json'
 import tk from '../data/tk/639-1.json'
+import tl from '../data/tl/639-1.json'
+import tn from '../data/tn/639-1.json'
+import to from '../data/to/639-1.json'
 import tr from '../data/tr/639-1.json'
+import ts from '../data/ts/639-1.json'
+import tt from '../data/tt/639-1.json'
 import tw from '../data/tw/639-1.json'
+import ty from '../data/ty/639-1.json'
 import ug from '../data/ug/639-1.json'
 import uk from '../data/uk/639-1.json'
 import ur from '../data/ur/639-1.json'
@@ -183,4 +183,189 @@ import za from '../data/za/639-1.json'
 import zh from '../data/zh/639-1.json'
 import zu from '../data/zu/639-1.json'
 
-export default { aa, ab, af, ak, am, ar, an, as, av, ae, ay, az, ba, bm, be, bn, bi, bo, bs, br, bg, ca, cs, ch, ce, cu, cv, kw, co, cr, cy, da, de, dv, dz, el, en, eo, et, eu, ee, fo, fa, fj, fi, fr, fy, ff, gd, ga, gl, gv, gn, gu, ht, ha, sh, he, hz, hi, ho, hr, hu, hy, ig, io, ii, iu, ie, ia, id, ik, is, it, jv, ja, kl, kn, ks, ka, kr, kk, km, ki, rw, ky, kv, kg, ko, kj, ku, lo, la, lv, li, ln, lt, lb, lu, lg, mh, ml, mr, mk, mg, mt, mn, mi, ms, my, na, nv, nr, nd, ng, ne, nl, nn, nb, no, ny, oc, oj, or, om, os, pa, pi, pl, pt, ps, qu, rm, ro, rn, ru, sg, sa, si, sk, sl, se, sm, sn, sd, so, st, es, sq, sc, sr, ss, su, sw, sv, ty, ta, tt, te, tg, tl, th, ti, to, tn, ts, tk, tr, tw, ug, uk, ur, uz, ve, vi, vo, wa, wo, xh, yi, yo, za, zh, zu }
+export default {
+  aa,
+  ab,
+  af,
+  ak,
+  am,
+  ar,
+  an,
+  as,
+  av,
+  ae,
+  ay,
+  az,
+  ba,
+  bm,
+  be,
+  bn,
+  bi,
+  bo,
+  bs,
+  br,
+  bg,
+  ca,
+  cs,
+  ch,
+  ce,
+  cu,
+  cv,
+  kw,
+  co,
+  cr,
+  cy,
+  da,
+  de,
+  dv,
+  dz,
+  el,
+  en,
+  eo,
+  et,
+  eu,
+  ee,
+  fo,
+  fa,
+  fj,
+  fi,
+  fr,
+  fy,
+  ff,
+  gd,
+  ga,
+  gl,
+  gv,
+  gn,
+  gu,
+  ht,
+  ha,
+  sh,
+  he,
+  hz,
+  hi,
+  ho,
+  hr,
+  hu,
+  hy,
+  ig,
+  io,
+  ii,
+  iu,
+  ie,
+  ia,
+  id,
+  ik,
+  is,
+  it,
+  jv,
+  ja,
+  kl,
+  kn,
+  ks,
+  ka,
+  kr,
+  kk,
+  km,
+  ki,
+  rw,
+  ky,
+  kv,
+  kg,
+  ko,
+  kj,
+  ku,
+  lo,
+  la,
+  lv,
+  li,
+  ln,
+  lt,
+  lb,
+  lu,
+  lg,
+  mh,
+  ml,
+  mr,
+  mk,
+  mg,
+  mt,
+  mn,
+  mi,
+  ms,
+  my,
+  na,
+  nv,
+  nr,
+  nd,
+  ng,
+  ne,
+  nl,
+  nn,
+  nb,
+  no,
+  ny,
+  oc,
+  oj,
+  or,
+  om,
+  os,
+  pa,
+  pi,
+  pl,
+  pt,
+  ps,
+  qu,
+  rm,
+  ro,
+  rn,
+  ru,
+  sg,
+  sa,
+  si,
+  sk,
+  sl,
+  se,
+  sm,
+  sn,
+  sd,
+  so,
+  st,
+  es,
+  sq,
+  sc,
+  sr,
+  ss,
+  su,
+  sw,
+  sv,
+  ty,
+  ta,
+  tt,
+  te,
+  tg,
+  tl,
+  th,
+  ti,
+  to,
+  tn,
+  ts,
+  tk,
+  tr,
+  tw,
+  ug,
+  uk,
+  ur,
+  uz,
+  ve,
+  vi,
+  vo,
+  wa,
+  wo,
+  xh,
+  yi,
+  yo,
+  za,
+  zh,
+  zu,
+}
